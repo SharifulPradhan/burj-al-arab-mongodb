18badce0c9346fa4b172b964c58bdc72102aee03
WebBygger <sharifulpradhanhridoy@gmail.com>
2021-03-27 16:16:24 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA3fin0TbMmkQwvxwldtE1zjwOPKvH2cDE",
+  authDomain: "burj-al-arab-af163.firebaseapp.com",
+  projectId: "burj-al-arab-af163",
+  storageBucket: "burj-al-arab-af163.appspot.com",
+  messagingSenderId: "765942082184",
+  appId: "1:765942082184:web:f37fd8bc712999cd500d3d"
+};
+export default firebaseConfig;
